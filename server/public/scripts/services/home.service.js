2843817a806aedadfee1008f8aa35c5dbf47d928
David Clark <primecomp@Davids-MacBook-Pro.local>
2018-03-13 09:01:01 -0500

    added more images, some styling and text

--- server/public/scripts/services/home.service.js
+++ server/public/scripts/services/home.service.js
@@ -2,5 +2,9 @@ myApp.service('HomeService', ['$http', '$location', function($http, $location){
     console.log('HomeService Loaded');
     var self = this;
 
-    self.siteIntro = `This is how the site works, words........ more words.......`;
+    self.siteIntro = `What we do: This site is dedicated to matching gamers with other gamers in their area looking for a battle or quest or just a stroll through a dungeon. 
+    We use maps, we use emails, but the best tool for finding a game is joining or creating a local gamer Fellowship. 
+    Fellowships are local groups of gamers you can connect with through this site and organize customized games and times.
+
+    `;
 }]);
