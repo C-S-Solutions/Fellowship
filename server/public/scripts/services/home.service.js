0c68f9d0478e89ca14cf884b71b4deb18c92732a
David Clark <primecomp@Davids-MacBook-Pro.local>
2018-04-26 13:54:00 -0500

    changed intro message

--- server/public/scripts/services/home.service.js
+++ server/public/scripts/services/home.service.js
@@ -2,10 +2,7 @@ myApp.service('HomeService', ['$http', '$location', function($http, $location){
     console.log('HomeService Loaded');
     var self = this;
 
-    self.siteIntro = `This site is dedicated to matching gamers with other gamers in their area looking for a battle or quest or just a stroll through a dungeon. 
-    We use maps, we use emails, but the best tool for finding a game is joining or creating a local gamer Fellowship. 
-    Fellowships are local groups of gamers you can connect with through this site and organize customized games and times.
-    `;
+    self.siteIntro = `This site is dedicated to all things tabletop gamers. Have some opinions about a GAME, check out the forum. Just finish painting a model and want to display it, upload in the gallery. Write some fan fiction, submit it and we will post it or if you want to check out what other fans are writing, take a tour of the writers bazaar.Want to checkout the latest battle reports, we have liinks and our commentary. Looking for a podcast to suit your interests, we got that too!`;
 
     self.weeklyBannerHeadline = `The Tau codex drops, time for a fish fry!`
     self.weekelyBannerBody = `Click on Opinions to:
